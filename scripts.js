dba12e6139e861cce9290146ae3de88261b7f63a
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-02-25 09:28:04 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-let userInput;
+/*let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
 }
+*/
+
+let userInput;
+userInput = prompt("What color is the sky?", "Type your answer here.");
+if ( userInput === "blue" ){
+  $("#response").html("You're smarter than a fifth grader :D");
+} else {
+  $("#response").html("Go back to kindergarten >:();
+                      }
