0860ac9ebbd958bd6954a2d1a35328efe2e16a34
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-02-25 09:30:49 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,8 +9,8 @@ if ( userInput === "burrito" ) {
 
 let userInput;
 userInput = prompt("What color is the sky?", "Type your answer here.");
-if ( userInput === "blue" ){
+if ( userInput === "blue" ) {
   $("#response").html("You're smarter than a fifth grader!");
 } else {
   $("#response").html("Go back to kindergarten.);
-                      }
+}
