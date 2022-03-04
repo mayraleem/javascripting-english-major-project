0c49653b2dd8c1420edf4963b41ec797e7a6f76c
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-03-04 09:32:19 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -27,9 +27,9 @@ blackBeans = "black";
 makeABurrito(blackBeans);
 */
 
-let arrayOfStrings, arrayOfNumbers, arrayMixed;
+/*let arrayOfStrings, arrayOfNumbers, arrayMixed;
 arrayOfStrings = ["a", "b", "c"};
 arrayOfNumbers = [1, 2, 3];
 arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
 $(#response).html(arrayOfStrings[2]);
-                  
+ */                 
