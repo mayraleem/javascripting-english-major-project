b4df38e938e7348895ff026fd52ccc37c27f57f3
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-03-11 23:02:40 -0500

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -8,10 +8,22 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     });
 tileLayer.addTo(firstMap);
 firstMap.setView([40.730833, -73.9975], 16);
-let bobstLibrary;
+/*let bobstLibrary;
 bobstLibrary = L.marker([40.729444, -73.997222]);
 bobstLibrary.addTo(firstMap);
 bobstLibrary.bindPopup("This is Bobst Library.");
 let bobstCoords;
 bobstCoords = L.latLng(40.729444, -73.997222);
 firstMap.panTo(bobstCoords);
+*/
+let washingtonSquarePark, washingtonSquareParkMarker;
+let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
+washingtonSquarePark = L.latLng(40.730833, -73.9975);
+washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
+tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974], 
+                 [40.7309, -73.9974], [40.7309, -73.9976]];
+tenThousandthPolygon = L.polygon(tenThousandth, {
+                                  color: "#268bd2",
+                                  fillColor: "#fdf6e3"
+                                }
+                        ).addTo(firstMap);
