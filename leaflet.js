13b6af7214525933982841681e8e65b14ce80593
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-03-25 09:08:34 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -6,7 +6,7 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
       subdomains: "abcd",
       maxZoom: 18
     });
-tileLayer.addTo(firstMap);
+/*tileLayer.addTo(firstMap);
 firstMap.setView([40.730833, -73.9975], 16);
 let bobstLibrary;
 bobstLibrary = L.marker([40.729444, -73.997222]);
@@ -15,7 +15,7 @@ bobstLibrary.bindPopup("This is Bobst Library.");
 let bobstCoords;
 bobstCoords = L.latLng(40.729444, -73.997222);
 firstMap.panTo(bobstCoords);
-
+*/
 let washingtonSquarePark, washingtonSquareParkMarker;
 let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
 washingtonSquarePark = L.latLng(40.730833, -73.9975);
