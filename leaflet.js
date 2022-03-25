7369521f3629812ec79343764e224ba1957599cf
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-03-25 10:38:24 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -9,7 +9,7 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 tileLayer.addTo(firstMap);
 firstMap.setView([40.730833, -73.9975], 16);
 
-let bobstLibrary, bobstLibraryMarker, washingtonSquarePark, washingtonSquareParkMarker;
+/*let bobstLibrary, bobstLibraryMarker, washingtonSquarePark, washingtonSquareParkMarker;
 bobstLibrary = L.latLng(40.729444, -73.997222);
 bobstLibraryMarker = L.marker(bobstLibrary).bindPopup("This is Bobst Library.").addTo(firstMap);
 washingtonSquarePark = L.latLng(40.730833, -73.9975);
@@ -23,7 +23,7 @@ circle = L.circle(washingtonSquarePark, bobstLibrary {radius: 100,
                                                       fillOpacity: 0.25}
           ).addTo(firstMap);
 
-/*let brooklynMuseum, brooklynMuseumMarker;
+let brooklynMuseum, brooklynMuseumMarker;
 brooklynMuseum = L.latLng(40.6712, -73.9639);
 brooklynMuseumMarker = L.marker(brooklynMuseum).bindPopup("This is the Brooklyn Museum.").addTo(firstMap);
 
