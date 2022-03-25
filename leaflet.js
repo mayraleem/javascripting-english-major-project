c9424748cf5f8b13d16a06bf601cfd7d4b41c589
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-03-25 16:48:18 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -24,10 +24,10 @@ circle = L.circle(bobstLibrary, {radius: 200,
           ).addTo(firstMap);
 let circle;
 circle = L.circle(washingtonSquarePark, {radius: 200,
-                                 color: "#859900",
-                                 fillColor: "#cb4b16",
-                                 opacity: 0.9,
-                                 fillOpacity: 0.25}
+                                         color: "#859900",
+                                         fillColor: "#cb4b16",
+                                         opacity: 0.9,
+                                         fillOpacity: 0.25}
           ).addTo(firstMap);
 
 /* let brooklynMuseum, brooklynMuseumMarker, theMet, theMetMarker, moma, momaMarker;
