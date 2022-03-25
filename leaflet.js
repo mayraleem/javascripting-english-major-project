7e160df8f22e6bd40341d330748b08099ce3385e
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-03-25 09:45:29 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,3 +1,23 @@
+let firstMap;
+firstMap = L.map("first-map");
+let firstMap, tileLayer;
+firstMap = L.map("first-map");
+tileLayer =
+L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+      attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
+      subdomains: "abcd",
+      maxZoom: 18
+    });
+tileLayer.addTo(firstMap);
+firstMap.setView([40.730833, -73.9975], 16);
+
+let bobstLibrary;
+bobstLibrary = L.marker([40.729444, -73.997222]);
+bobstLibrary.addTo(firstMap);
+bobstLibrary.bindPopup("This is Bobst Library.");
+
+
+
 /*let firstMap, tileLayer;
 firstMap = L.map("first-map");
 tileLayer =
