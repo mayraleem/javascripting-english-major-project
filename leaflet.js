24a3f7faf892f628c122005dc0a8ab91111a0425
mayraleem <95361967+mayraleem@users.noreply.github.com>
2022-03-25 11:11:39 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -16,7 +16,7 @@ washingtonSquarePark = L.latLng(40.730833, -73.9975);
 washingtonSquareParkMarker = L.marker(washingtonSquarePark).bindPopup("This is Washington Square Park.").addTo(firstMap);
 
 /*let circle;
-circle = L.circle(washingtonSquarePark, bobstLibrary {radius: 100,
+circle = L.circle(washingtonSquarePark, bobstLibrary, {radius: 100,
                                                       color: "#859900",
                                                       fillColor: "#cb4b16",
                                                       opacity: 0.9,
@@ -28,7 +28,7 @@ brooklynMuseum = L.latLng(40.6712, -73.9639);
 brooklynMuseumMarker = L.marker(brooklynMuseum).bindPopup("This is the Brooklyn Museum.").addTo(firstMap);
 
 let circle;
-circle = L.circle(brooklynMuseum {radius: 150,
+circle = L.circle(brooklynMuseum, {radius: 150,
                                     color: "#ff008c",
                                     fillColor: "#859900",
                                     opacity: 0.9,
